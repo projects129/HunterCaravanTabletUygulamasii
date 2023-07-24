a2a1d4778f13224f7a57ef7e9e37d59fba446df6
Seda Nur Önder <136471199+sedanuronderrrr@users.noreply.github.com>
2023-07-24 16:38:59 +0300

    update date and time

--- app/src/main/java/com/caravan/huntercaravantabletuygulamasii/fragments/AnasayfaFragment.java
+++ app/src/main/java/com/caravan/huntercaravantabletuygulamasii/fragments/AnasayfaFragment.java
@@ -24,6 +24,9 @@ import android.widget.TextView;
 import com.caravan.huntercaravantabletuygulamasii.MainActivity;
 import com.caravan.huntercaravantabletuygulamasii.R;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -41,9 +44,10 @@ public class AnasayfaFragment extends Fragment {
   int temizsugelensayi= 5;
     TextView saat;
     TextView dakika;
-    TextView Gun;
+    TextView Gun,guntext;
     TextView Ay;
     TextView Yil;
+    String[] splitDate;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,11 +75,11 @@ public class AnasayfaFragment extends Fragment {
         String day = Timee.getString("gun","00");
         String ay = Timee.getString("ay","00");
         String yil = Timee.getString("yil","00");
-        saat.setText(time);
-        dakika.setText(dk);
-        Gun.setText(day);
-        Ay.setText(ay);
-        Yil.setText(yil);
+      //  saat.setText(time);
+       // dakika.setText(dk);
+      //  Gun.setText(day);
+     //   Ay.setText(ay);
+       // Yil.setText(yil);
 
     }
 
@@ -90,9 +94,16 @@ public class AnasayfaFragment extends Fragment {
         Gun = view.findViewById(R.id.gun);
         Ay = view.findViewById(R.id.ay);
         Yil = view.findViewById(R.id.yil);
+        guntext = view.findViewById(R.id.guntext);
+
+        //**************************************************
 
 
 
+
+
+
+       //**********************************************
 
         cl_water_txt=(TextView)view.findViewById(R.id.textView67);
         dt_water_txt=(TextView)view.findViewById(R.id.textView63);
@@ -112,7 +123,99 @@ public class AnasayfaFragment extends Fragment {
 
         Thread_refresh = new Thread(new AnasayfaFragment.refresh_Task());
         Thread_refresh.start();
+
+
+        loadLocale();
     }
+
+
+    private void setLocale(String s) {
+        Locale locale = new Locale(s);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getResources().updateConfiguration(config,getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getActivity().getSharedPreferences("setting",MODE_PRIVATE).edit();
+        editor.putString("my lang",s);
+        editor.putString("languagetext",s);
+        if(s.equals("en")){
+            editor.putInt("image", R.drawable.ingilizce);
+            Date curentTime = Calendar.getInstance().getTime();
+            String formattedDate =   DateFormat.getDateInstance(DateFormat.FULL).format(curentTime);
+            splitDate = formattedDate.split(",");
+           String[] splitDate1 = formattedDate.split("\\s+");
+            Ay.setText(splitDate[1]);
+           Gun.setText("");
+
+             Yil.setText(splitDate[2]);
+            guntext.setText(splitDate[0]);
+
+            DateFormat dateFormat1 = DateFormat.getTimeInstance(DateFormat.DEFAULT, locale);
+            String date = dateFormat1.format(new Date());
+            String[]  splittime = date.split(":");
+            saat.setText(splittime[0].trim());
+            dakika.setText(splittime[1].trim());
+            Log.e("saaaat",splittime[0].trim());
+        }
+        else if(s.equals("tr")){
+            editor.putInt("image", R.drawable.turkiye);
+            Date curentTime = Calendar.getInstance().getTime();
+            String formattedDate =   DateFormat.getDateInstance(DateFormat.FULL).format(curentTime);
+            splitDate = formattedDate.split("\\s+");
+            Ay.setText(splitDate[1].trim());
+            Gun.setText(splitDate[0].trim());
+            Yil.setText(splitDate[2].trim());
+            guntext.setText(splitDate[3].trim());
+
+            DateFormat dateFormat1 = DateFormat.getTimeInstance(DateFormat.DEFAULT, locale);
+            String date = dateFormat1.format(new Date());
+            String[]  splittime = date.split(":");
+            saat.setText(splittime[0].trim());
+            dakika.setText(splittime[1].trim());
+            Log.e("saaaat",splittime[0].trim());
+
+
+        }
+        else {
+
+            editor.putInt("image", R.drawable.almanca);
+            Date curentTime = Calendar.getInstance().getTime();
+            String formattedDate =   DateFormat.getDateInstance(DateFormat.FULL).format(curentTime);
+            splitDate = formattedDate.split(",");
+            String[] splitDate1 = formattedDate.split("\\s+");
+            Ay.setText(splitDate1[2]);
+            Gun.setText(splitDate1[1]);
+
+           Yil.setText(splitDate1[3]);
+            guntext.setText(splitDate[0]);
+
+            DateFormat dateFormat1 = DateFormat.getTimeInstance(DateFormat.DEFAULT, locale);
+            String date = dateFormat1.format(new Date());
+            String[]  splittime = date.split(":");
+            saat.setText(splittime[0].trim());
+            dakika.setText(splittime[1].trim());
+            Log.e("saaaat",splittime[0].trim());
+
+        }
+        editor.apply();
+
+    }
+    public void loadLocale(){
+        SharedPreferences prefs = getActivity().getSharedPreferences("setting",MODE_PRIVATE);
+        String language = prefs.getString("my lang","");
+        setLocale(language);
+    }
+
+
+
+
+
+
+
+
+
+
+
 
     class refresh_Task implements Runnable {
         public void run() {
